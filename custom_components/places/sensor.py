92404b96f4b46d46f179562626328e37963e8ec8
Snuffy2 <Snuffy2@users.noreply.github.com>
2022-09-02 22:21:06 -0400

    Clean up the initial description comments

--- custom_components/places/sensor.py
+++ custom_components/places/sensor.py
@@ -2,76 +2,15 @@
 Place Support for OpenStreetMap Geocode sensors.
 
 Original Author:  Jim Thompson
+Subsequent Author: Ian Richardson
 
-Current Version:  1.8  20210125 - iantrich
-
-20180330 - Initial Release
-         - Event driven and timed updates
-         - Subscribes to DeviceTracker state update events
-         - State display options are (default "zone, place"):
-           "zone, place, street_number, street, city, county, state, postal_code, country, formatted_address"
-         - If state display options are specified in the configuration.yaml file:
-           - The state display string begins as a null and appends the following in order:
-             - 'zone' - as defined in the device_tracker entity
-             - If 'place' is included in the options string, a concatenated string is created with the following attributes
-               - place_name, 
-               - place_category, 
-               - place_type, 
-               - place_neighbourhood, 
-               - street number, 
-               - street
-               - If 'street_number' and 'street' are also in the options string, they are ignored
-             - If 'place' is NOT included:
-               - If 'street_number' is included in the options string, the 'street number' will be appended to the display string
-               - If 'street' is included in the options string, the 'street name' will be appended to the display string
-            - If specified in the options string, the following attributes are also appended in order:
-              - "city"
-              - "county"
-              - "state'
-              - "postal_code"
-              - "country"
-              - "formatted_address"
-           - If for some reason the option string is null at this point, the following values are concatenated:
-             - "zone"
-             - "street"
-             - "city"
-         - Whenever the actual 'state' changes, this sensor fires a custom event named 'places_state_update' containing:
-           - entity
-           - to_state
-           - from_state
-           - place_name
-           - direction
-           - distance_from_home
-           - devicetracker_zone
-           - latitude
-           - longitude
-         - Added Map_link option to generate a Google or Apple Maps link to the users current location
-20180509 - Updated to support new option value of "do_not_reorder" to disable the automatic ordered display of any specified options
-         - If "do_not_reorder" appears anywhere in the list of comma delimited options, the state display will be built 
-           using the order of options as they are specified in the options config value.
-           ie:  options: street, street_number, do_not_reorder, postal_code, city, country 
-           will result in a state comprised of: 
-                <street>, <street_number>, <postal_code>, <city>, <country> 
-           without the "do_not_reorder" option, it would be:
-                <street_number>, <street>, <postal_code>, <city>, <country>
-         - The following attributes can be specified in any order for building the display string manually:
-            - do_not_reorder
-            - place_type, place_name, place_category, place_neighbourhood, street_number, street, city,
-            - postal_town, state, region, county, country, postal_code, formatted_address
-            Notes:  All options must be specified in lower case.  
-                    State and Region return the same data (so only use one of them).
-         - Also added 'options' to the attribute list that gets populated by this sensor (to make it easier to see why a specific state is being generated)
-20180510 - Fixed stupid bug introduced yesterday.  Converted display options from string to list.
-
-           
 Description:
   Provides a sensor with a variable state consisting of reverse geocode (place) details for a linked device_tracker entity that provides GPS co-ordinates (ie owntracks, icloud)
   Optionally allows you to specify a 'home_zone' for each device and calculates distance from home and direction of travel.
-  The displayed state adds a time stamp "(since hh:mm)" so you can tell how long a person has been at a location.
   Configuration Instructions are below - as well as sample automations for notifications.
   
   The display options I have set for Sharon are "zone, place" so her state is displayed as:
-  - not_home, Richmond Hill GO Station, building, building, Beverley Acres, 6, Newkirk Road (since 18:44)
+  - not_home, Richmond Hill GO Station, building, building, Beverley Acres, 6, Newkirk Road
   There are a lot of additional attributes (beyond state) that are available which can be used in notifications, alerts, etc:
   (The "home latitude/longitudes" below have been randomized to protect her privacy)
 {
@@ -188,13 +127,9 @@ Sample generic automations.yaml snippet to send an iOS notify on any device stat
           hide_thumbnail: false
 
 
-Note:  The OpenStreetMap database is very flexible with regards to tag_names in their
-       database schema.  If you come across a set of co-ordinates that do not parse
-       properly, you can enable debug messages to see the actual JSON that is returned from the query.
+Note:  The OpenStreetMap database is very flexible with regards to tag_names in their database schema.  If you come across a set of co-ordinates that do not parse properly, you can enable debug messages to see the actual JSON that is returned from the query.
 
-Note:  The OpenStreetMap API requests that you include your valid e-mail address in each API call
-       if you are making a large numbers of requests.  They say that this information will be kept
-       confidential and only used to contact you in the event of a problem, see their Usage Policy for more details.
+Note:  The OpenStreetMap API requests that you include your valid e-mail address in each API call if you are making a large numbers of requests.  They say that this information will be kept confidential and only used to contact you in the event of a problem, see their Usage Policy for more details.
 
 Configuration.yaml:
   sensor places_jim:
@@ -207,12 +142,11 @@ Configuration.yaml:
       map_zoom: <1-20>                              (optional)
       option: <zone, place, street_number, street, city, county, state, postal_code, country, formatted_address>  (optional)
       
-The map link that gets generated for Google maps has a push pin marking the users location.
-The map link for Apple maps is centered on the users location - but without any marker.
+The map link that gets generated for Google & Apple maps has a push pin marking the users location.
       
 To enable detailed logging for this component, add the following to your configuration.yaml file
   logger:
-    default: warn
+    default: warning
     logs:
       custom_components.sensor.places: debug  
 
