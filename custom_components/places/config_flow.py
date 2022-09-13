d4be89968b4a4e4af254ab4d9e8b5b597086ea74
Snuffy2 <Snuffy2@users.noreply.github.com>
2022-09-12 23:52:05 -0400

    :art: Format Python code with isort & Black Code Formatter (#107)

--- custom_components/places/config_flow.py
+++ custom_components/places/config_flow.py
@@ -51,7 +51,7 @@ DATA_SCHEMA = vol.Schema(
         vol.Optional(CONF_MAP_PROVIDER, default=DEFAULT_MAP_PROVIDER): str,
         vol.Optional(CONF_MAP_ZOOM, default=DEFAULT_MAP_ZOOM): str,
         vol.Optional(CONF_LANGUAGE, default=DEFAULT_LANGUAGE): str,
-        #vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
+        # vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
         vol.Optional(CONF_EXTENDED_ATTR, default=DEFAULT_EXTENDED_ATTR): bool,
     }
 )
