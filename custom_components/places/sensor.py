c9c7a6c7b5eab0ab3492da7ad40f85af68e0c7ca
Snuffy2 <Snuffy2@users.noreply.github.com>
2022-09-16 19:44:09 -0400

    :art: Format Python code with isort & Black Code Formatter (#132)

--- custom_components/places/sensor.py
+++ custom_components/places/sensor.py
@@ -119,7 +119,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_MAP_PROVIDER, default=DEFAULT_MAP_PROVIDER): cv.string,
         vol.Optional(CONF_MAP_ZOOM, default=DEFAULT_MAP_ZOOM): cv.positive_int,
         vol.Optional(CONF_LANGUAGE, default=DEFAULT_LANGUAGE): cv.string,
-        #vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
+        # vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
         vol.Optional(CONF_EXTENDED_ATTR, default=DEFAULT_EXTENDED_ATTR): cv.boolean,
     }
 )
