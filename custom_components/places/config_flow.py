25c566248cc2d687ff5f956b5eccd04d52187c61
Snuffy2 <Snuffy2@users.noreply.github.com>
2022-09-21 08:42:24 -0400

    Update config_flow.py

--- custom_components/places/config_flow.py
+++ custom_components/places/config_flow.py
@@ -190,6 +190,7 @@ class PlacesOptionsFlowHandler(config_entries.OptionsFlow):
             # )
             for m in dict(self.config_entry.data).keys():
                 user_input.setdefault(m, self.config_entry.data[m])
+                _LOGGER.debug("[Options Update] user_input[" + m + "]: '" + str(user_input.get(m)) + "'")
                 if user_input.get(m) == "":
                     user_input.pop(m)
             _LOGGER.debug("[Options Update] user_input: " + str(user_input))
